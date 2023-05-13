169edbdc47a8937e8d3f60aa7d4dc79323939d69
User <mirboqijonov@example.com>
2023-05-13 19:40:10 +0500

    lang

---
+++ Back_End/resources/views/service.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/service.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <nav id="navbar">
+
+        <div class="menu">
+            <input type="checkbox" id="check">
+            <div class="logo">
+                <a href="#">
+                    <img src="assets/images/logo.jpg" alt="" class="logo_img">
+        </a>
+            </div>
+            <ul>
+
+                <label class="btn cancel"><i class="fa fa-close"></i></label>
+                <li><a href="index.html" class="lang" key="" for="check">{{ __('app.home') }}</a></li>
+                <li><a href="index.html#about" class="lang" key="">{{ __('app.about') }}</a></li>
+                <li><a href="index.html#projects" class="lang" key="">{{ __('app.projects') }}</a></li>
+                <li><a href="#contact" class="lang" key="">{{ __('app.contact') }}</a></li>
+                <li><a href="#" class="lang" key="">{{ __('app.services') }}</a></li>
+                <!-- <li><a href="#" class="lang" key="products">Products{{ __('app.about') }}</a></li> -->
+                <div class="language_container">
+                    <a class="lang-button active_lang lang_border" data-lang="en">EN{{ __('app.about') }}</a>
+                    <a class="lang-button" data-lang="ru">RU{{ __('app.about') }}</a>
+                </div>
+            </ul>
+
+            <label for="check" class="btn bars"><i class="fa fa-bars"></i></label>
+        </div>
+        </div>
+    </nav>
+
+
+
+<div class="service_page_container">
+    <h1 class="lang service_title" key=""> {{ __('app.services') }}</h1>
+    <ul class="service_ul">
+        <li class="lang service_li" key="">{{ __('app.service_li1') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li2') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li3') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li4') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li5') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li6') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li7') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li8') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li9') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li10') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li11') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li12') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li13') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li14') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li15') }}</li>
+        <li class="lang service_li" key=""> {{ __('app.service_li16') }}</li>
+    </ul>
+</div>
+
+
+
+    <footer>
+        <div class="footer_container">
+            <div class="contact" id="contact">
+                <div class="title_footer lang" key=""> {{ __('app.contact') }}</div>
+                <div class="phone_container">
+                    <i class="fa fa-phone"></i>
+                    <div class="phones">
+                        <a href="tel:+998901225516">+998 90 122 55 16{{ __('app.about') }}</a>
+                    </div>
+                </div>
+                <div class="mail_container">
+                    <i class="fa fa-envelope"></i>
+                    <div class="mail">
+                        <a href="">Emc_technology@gmail.com{{ __('app.about') }}</a>
+                    </div>
+                </div>
+            </div>
+            <div class="time_location">
+                <div class="title_footer lang" key="">{{ __('app.job_time_title') }}</div>
+
+                <div class="time_container">
+                    <i class="fa fa-clock-o"></i>
+                    <div class="time">
+                        <p class="lang" key="">{{ __('app.job_time') }}</p>
+                    </div>
+                </div>
+                <!-- Каждый день
+            с 10:00 до 22:00 -->
+                <div class="location_container">
+                    <i class="fa fa-map-marker"></i>
+                    <div class="location">
+                        <p class="lang" key="">{{ __('app.location') }}</p>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="social_netvork">
+                <div class="title_footer lang" key="">{{ __('app.social_title') }}</div>
+
+                <div class="telegram_container">
+                    <i class="fa fa-telegram"></i>
+                    <div class="telegram">
+                        <a href="#">@Emc_technology{{ __('app.about') }}</a>
+                    </div>
+                </div>
+                <div class="instagram_container">
+                    <i class="fa fa-instagram"></i>
+                    <div class="instagram">
+                        <a href="">@Emc_technology{{ __('app.about') }}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </footer>
+    <div class="powered_by">
+        <div class="powered_by_text">
+            <P>© 2023 Powered by </P><a href="cmd-tech.uz"> CMD TECH{{ __('app.about') }}</a>
+        </div>
+    </div>
+
+    <script src="assets/js/company_logo_slider.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/carusel.js"></script>
+
+    <script src="assets/js/lang.js"></script>
+</body>
+</html>
